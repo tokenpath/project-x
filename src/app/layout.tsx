a8b3dd6d058fcab672419e0032290d403649e54f
tokenpath <controller.henry@gmail.com>
2025-02-01 20:36:59 +0100

    Initialize tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/components/provider/query-provider";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        <QueryProvider>
+        <ThemeProvider
       >
         {children}
+        </QueryProvider>
       </body>
     </html>
   );
