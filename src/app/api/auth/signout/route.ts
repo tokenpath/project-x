b27db5ac9af9204d11735de907f953b802676c7c
tokenpath <controller.henry@gmail.com>
2025-02-11 20:34:04 +0100

    Update sign out route and hook usage

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { cookies } from "next/headers";
 import { decrypt } from "@/lib/crypto";
+import { Session } from "@prisma/client";
 
 export async function POST() {
   try {
@@ -17,13 +18,13 @@ export async function POST() {
       );
     }
 
-    const decryptedSessionToken = await decrypt(authCookie.value);
+    const decryptedSessionToken = await decrypt<Session>(authCookie.value);
     if (!decryptedSessionToken) {
       throw new Error("Failed to decrypt session token");
     }
 
     await prisma.session.delete({
-      where: { sessionToken: decryptedSessionToken },
+      where: { id: decryptedSessionToken.id },
     });
 
     cookieStore.delete("auth");
--- src/components/navigation/client/signout-button.tsx
+++ src/components/navigation/client/signout-button.tsx
@@ -3,11 +3,19 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
 import { LogOut } from 'lucide-react';
+import { SignOutHook } from '@/hooks/auth/SignOutHook';
+import { useRouter } from 'next/navigation';
 
 const SignOutButton = () => {
+  const { mutate: signOut } = SignOutHook();
+  const router = useRouter()
+  const handleSignOut = () => {
+      signOut()
+      router.refresh()
+  }
 
   return (
-    <Button variant="outline" className="rounded-full font-semibold relative group">
+    <Button onClick={()=> handleSignOut()} variant="outline" className="rounded-full font-semibold relative group">
       <div className="absolute inset-0 bg-primary/30 rounded-full blur-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
       <LogOut className="w-5 h-5 mr-2"/> 
       Log Out
